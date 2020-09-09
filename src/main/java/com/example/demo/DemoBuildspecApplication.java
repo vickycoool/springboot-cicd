664b9bfbe123216076fa388af698b14f6228d99a
Bicky Choudhary <bicky.choudhary@wipro.com>
2020-09-09 09:53:01 +0530

    initial commit

---
+++ src/main/java/com/example/demo/DemoBuildspecApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBuildspecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBuildspecApplication.class, args);
+	}
+
+}
